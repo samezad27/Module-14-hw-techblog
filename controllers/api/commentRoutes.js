c05f3d8f13a4f5a0e7a8bcb64977c7212ed60d73
samuel ezadmanesh <samezad27@gmail.com>
2023-02-01 17:55:31 -0800

    almost finished routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,10 +1,20 @@
-
-const router = require('express').Router();
-const { Comment } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Comment } = require("../../models/");
+const withAuth = require("../../utils/auth");
 
 // TODO - create a POST route for creating a new comment
 // This should be a protected route, so you'll need to use the withAuth middleware
 
-module.exports = router;
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      body: req.body.body,
+      userId: req.session.userId,
+    });
+    res.json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,15 +1,25 @@
-
-const router = require('express').Router();
-const { Post } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models/");
+const withAuth = require("../../utils/auth");
 
 // TODO - create a POST route for creating a new post
 // This should be a protected route, so you'll need to use the withAuth middleware
 
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newPost = await Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      userId: req.session.userId,
+    });
+    res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // TODO - create a PUT route for updating a post's title or body
 // This should be a protected route, so you'll need to use the withAuth middleware
-
 
 // TODO - create a DELETE route for deleting a post with a specific id
 // This should be a protected route, so you'll need to use the withAuth middleware
